97f460a4e18dea7465665f44388193b56c84fc87
Levente Szilveszter <l3vi.sylvester@gmail.com>
2023-01-06 16:03:58 +0200

    Add files via upload
    added files via upload because by mistake I created Firstname_Lastname_Lab11 .NET MAUI project in different folder then the repo so it must be added manually

---
+++ Szilveszter_Levente_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Szilveszter_Levente_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Szilveszter_Levente_Lab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Szilveszter_Levente_Lab11.Models;
+
+namespace Szilveszter_Levente_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
